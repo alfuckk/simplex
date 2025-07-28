92c96ad2cf716db269df0861e2d9c12c578889c5
xiaxj-dev <xiaxuejin@jingshun.fun>
2025-07-28 19:44:14 +0800

    first commit

---
+++ app/geoip/internal/task/task.go
@@ -0,0 +1,27 @@
+package task
+
+import (
+	"simplex/app/geoip/internal/repository"
+	"simplex/pkg/jwt"
+	"simplex/pkg/log"
+	"simplex/pkg/sid"
+)
+
+type Task struct {
+	logger *log.Logger
+	sid    *sid.Sid
+	jwt    *jwt.JWT
+	tm     repository.Transaction
+}
+
+func NewTask(
+	tm repository.Transaction,
+	logger *log.Logger,
+	sid *sid.Sid,
+) *Task {
+	return &Task{
+		logger: logger,
+		sid:    sid,
+		tm:     tm,
+	}
+}
---
+++ app/user/cmd/server/wire/wire.go
@@ -0,0 +1,73 @@
+//go:build wireinject
+// +build wireinject
+
+package wire
+
+import (
+	"simplex/app/user/internal/handler"
+	"simplex/app/user/internal/job"
+	"simplex/app/user/internal/repository"
+	"simplex/app/user/internal/server"
+	"simplex/app/user/internal/service"
+	"simplex/pkg/app"
+	"simplex/pkg/jwt"
+	"simplex/pkg/log"
+	"simplex/pkg/server/http"
+	"simplex/pkg/sid"
+
+	"github.com/google/wire"
+	"github.com/spf13/viper"
+)
+
+var repositorySet = wire.NewSet(
+	repository.NewDB,
+	//repository.NewRedis,
+	//repository.NewMongo,
+	repository.NewRepository,
+	repository.NewTransaction,
+	repository.NewUserRepository,
+)
+
+var serviceSet = wire.NewSet(
+	service.NewService,
+	service.NewUserService,
+)
+
+var handlerSet = wire.NewSet(
+	handler.NewHandler,
+	handler.NewUserHandler,
+)
+
+var jobSet = wire.NewSet(
+	job.NewJob,
+	job.NewUserJob,
+)
+var serverSet = wire.NewSet(
+	server.NewHTTPServer,
+	server.NewJobServer,
+)
+
+// build App
+func newApp(
+	httpServer *http.Server,
+	jobServer *server.JobServer,
+	// task *server.Task,
+) *app.App {
+	return app.NewApp(
+		app.WithServer(httpServer, jobServer),
+		app.WithName("demo-server"),
+	)
+}
+
+func NewWire(*viper.Viper, *log.Logger) (*app.App, func(), error) {
+	panic(wire.Build(
+		repositorySet,
+		serviceSet,
+		handlerSet,
+		jobSet,
+		serverSet,
+		sid.NewSid,
+		jwt.NewJwt,
+		newApp,
+	))
+}
